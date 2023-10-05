5207cf8d72b30d60466ab0f65a72001e97c010a0
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2023-10-05 23:21:37 +0530

    connected MongoDB and Setting Up Backend

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const connectDatabase = require("./db/database");
 const app = express();
 //require("dotenv").config();
 require("dotenv").config({ path: "./config/.env" });
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
 
 app.get("*", (req, res) => {
 	console.log("HHIHI");
@@ -11,4 +20,5 @@ app.get("*", (req, res) => {
 
 app.listen(PORT, () => {
 	console.log(`Server is running on ${PORT}`);
+	connectDatabase();
 });
