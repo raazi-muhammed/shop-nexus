37df4fc99b7daebb26a5a1a72447304cf22b02ea
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2023-11-10 22:57:16 +0530

    better fix for chick size limit

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./feature/cart/cartSlice";
 import wishListReducer from "./feature/wishList/wishListSlice";
+
 import userDataReducer from "./feature/userData/userDataSlice";
 import orderDataReducer from "./feature/order/orderSlice";
 import searchOptionsReducer from "./feature/search/searchOptionsSlice";
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,18 +6,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react(), nodePolyfills()],
 	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes("node_modules")) {
-						return id
-							.toString()
-							.split("node_modules/")[1]
-							.split("/")[0]
-							.toString();
-					}
-				},
-			},
-		},
+		chunkSizeWarningLimit: 2600,
 	},
 });
